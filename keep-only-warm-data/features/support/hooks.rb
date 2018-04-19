f725df1fe38f44e43b670f2598387aa3e2ec3674
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-04-19 15:40:53 +0200

    remove docker-compose pull from hooks.rb

--- keep-only-warm-data/features/support/hooks.rb
+++ keep-only-warm-data/features/support/hooks.rb
@@ -3,9 +3,6 @@ require 'open3'
 Before do |scenario|
   # this fixes the input device is not a TTY .. see https://github.com/docker/compose/issues/5696
   ENV['COMPOSE_INTERACTIVE_NO_CLI'] = '1'
-
-  puts "Pull latests images from Docker Hub"
-  Open3.capture3('docker-compose pull')
 end
 
 After do |scenario|
