c2a0e822dd47c75c004132c366fe989606af6ffe
Eric Trousset <etrousset@kaliop.com>
2018-04-20 10:41:26 +0200

    Revert "freeboard: howto to use Kuzzle Datasource"
    This reverts commit a3cbab4cc9992d40809ca81fde6d39f3348ab1b3.

--- freeboard-kuzzle-datasource/scripts/fb-kuzzle-fixtures.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo Creating fb-howto-index
-wget --method=POST http://localhost:7512/fb-howto-index/_create?pretty -O - -q; echo
-
-echo Creating fb-howto-collection
-wget --method=PUT http://localhost:7512/fb-howto-index/fb-howto-collection?pretty -O - -q; echo
