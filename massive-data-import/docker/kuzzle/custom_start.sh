766f63088b76aa4ac4b949c649eb93054154fe50
Adrien Maret <amaret93@gmail.com>
2018-04-25 11:21:42 +0200

    Revert "Add How-To massive-data-import (#2)" (#13)
    This reverts commit 64d46f2aecd1e4889c017440bc6d3bca226d0f66.

--- massive-data-import/docker/kuzzle/custom_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-npm --unsafe --prefix /scripts install /scripts
-
-# Allow user to remove node_modules without sudo
-chmod -R 777 /scripts/node_modules
-rmdir /scripts/etc
-
-kuzzle start --mappings /yellow_taxi/yellow_taxi_mapping.json
--- massive-data-import/docker/push_images.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Push to Docker Hub"
-docker push kuzzleio/howto-massiveimport-kuzzle
