9103a0a14cfffc128de308f77a004c58821229d7
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-05-03 12:13:06 +0200

    Update gunziping

--- massive-data-import/docker/build_images.sh
+++ massive-data-import/docker/build_images.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-echo "Kuzzle: Move Yellow Taxi data"
-mv ../../samples/yellow_taxi/yellow_taxi_data.csv.gz kuzzle/yellow_taxi/.
-
 echo "Kuzzle: Unziping Yellow Taxi data"
-gunzip kuzzle/yellow_taxi/yellow_taxi_data.csv.gz
+gunzip -k ../../samples/yellow_taxi/yellow_taxi_data.csv.gz
+
+echo "Kuzzle: Move Yellow Taxi data"
+mv ../../samples/yellow_taxi/yellow_taxi_data.csv kuzzle/yellow_taxi/.
 
 echo "Kuzzle: Build docker image"
 docker build -t kuzzleio/howto-massiveimport-kuzzle kuzzle/.
-
-echo "Kuzzle: Ziping Yellow Taxi data"
-gzip -9 kuzzle/yellow_taxi/yellow_taxi_data.csv
-
-echo "Kuzzle: Move Yellow Taxi data"
-mv kuzzle/yellow_taxi/yellow_taxi_data.csv.gz ../../samples/yellow_taxi/.
