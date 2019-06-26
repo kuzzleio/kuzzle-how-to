8b795bb484c77c68d105dcd1d80f139f583a99a1
Berthier Esteban <44427849+berthieresteban@users.noreply.github.com>
2019-06-26 09:49:31 +0200

    Update todo-MVC-Kuzzle-VueJS/src/store/lists.js
    Co-Authored-By: Adrien Maret <amaret93@gmail.com>

--- todo-MVC-Kuzzle-VueJS/src/store/lists.js
+++ todo-MVC-Kuzzle-VueJS/src/store/lists.js
@@ -57,7 +57,7 @@ const actions = {
   FETCH_LISTS: async ({ commit, dispatch }, { kuzzle }) => {
     commit('RESET_LISTS');
     const { collections } = await kuzzle.collection.list(state.groupList);
-    lists.collections.forEach(list => {
+    collections.forEach({ name } => {
       commit('ADD_LIST', list.name);
     });
     const listName = state.lists[0];
